15c67234deb2501a7fbb8e285f1e78d02413f2bf
Marek Rozmus <marek.rozmus@sandstream.pl>
2019-10-22 14:30:59 +0200

    Update SwipeableList test

--- src/__tests__/SwipeableList.test.js
+++ src/__tests__/SwipeableList.test.js
@@ -66,3 +66,38 @@ test('blocking swipe on scroll', () => {
   swipeLeftMouse(listItem);
   expect(callbackLeft).toHaveBeenCalledTimes(2);
 });
+
+test('blocking swipe on scrollElement scroll', () => {
+  const callbackLeft = jest.fn();
+
+  const { queryAllByTestId } = render(
+    <SwipeableList scrollElement={window.document}>
+      <SwipeableListItem
+        swipeLeft={{
+          content: <span>Left swipe content</span>,
+          action: callbackLeft
+        }}
+      >
+        <span>Item content 1</span>
+      </SwipeableListItem>
+    </SwipeableList>
+  );
+
+  const listItem = queryAllByTestId('content')[0];
+
+  // try to swipe - should work
+  swipeLeftMouse(listItem);
+  expect(callbackLeft).toHaveBeenCalledTimes(1);
+
+  fireEvent.scroll(window.document, { target: { scrollY: 100 } });
+
+  // block swipe should be on
+  swipeLeftMouse(listItem);
+  expect(callbackLeft).toHaveBeenCalledTimes(1);
+
+  fireEvent.mouseUp(window.document);
+
+  // swiping should be possible again
+  swipeLeftMouse(listItem);
+  expect(callbackLeft).toHaveBeenCalledTimes(2);
+});
